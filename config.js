5e29836fad1995ff36d668be19088d7950c905c0
NachiLuong <lqtrung20122001@gmail.com>
2021-12-16 22:12:35 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Yêu tớ nha",
+    introTitle: 'Chào người yêu của NaChiLuong',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao tớ cứ đi nhầm đường
+    Lạc vào tim cậu lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '😘Yêu nha😘',
+    title: 'Phải chăng cậu đã yêu tớ ngay từ cái nhìn đầu tiên 🥰, Phải chăng cậu đã say tớ ngay từ lúc thấy nụ cười ấy',
+    desc: 'Làm người yêu Nachi Luong nha ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
+    btnReply: 'Gửi cho NaChi Luong',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/lqtrung.01/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
