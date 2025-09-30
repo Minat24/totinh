8b35f1d633f405c76078fc1986dde5c69081d104
Minat24 <minhnhat241203@gmail.com>
2025-09-30 14:02:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Yêu tớ nha",
-    introTitle: 'Chào người yêu của tôi',
+    titleWeb: "Yêu anh nha",
+    introTitle: 'Chào người yêu của anh',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao tớ cứ đi nhầm đường
-    Lạc vào tim cậu lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '😘Yêu nha😘',
-    title: 'Phải chăng cậu đã yêu tớ ngay từ cái nhìn đầu tiên 🥰, Phải chăng cậu đã say tớ ngay từ lúc thấy nụ cười ấy',
-    desc: 'Làm người yêu tôi   nha ',
+    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên 🥰, Phải chăng em đã say anh ngay từ lúc thấy nụ cười ấy',
+    desc: 'Làm người yêu anh nha ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
-    btnReply: 'Gửi cho tôi Luong',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Thứ 7 này anh qua đón em đi chơi nha.',
+    btnAccept: 'Dạaaa anhh <3',
     messLink: 
 }
